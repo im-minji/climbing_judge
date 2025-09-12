aa9e18cb85b7102f1e22a33a59a086f827ed3164
“im-minji” <mjlim514@gmail.com>
2025-09-13 00:21:26 +0900

    routers/competitions.py date 데이터 형식  수정

--- app/routers/competitions.py
+++ app/routers/competitions.py
@@ -5,7 +5,7 @@ router = APIRouter()
 
 @router.post("/competitions")
 def create_compeition(competition: CompetitionCreate):
-    competition_data = competition.model_dump()
+    competition_data = competition.model_dump(mode="json")
     response = supabase.from_("compeitions").insert(competition_data).execute()
     return response.data
 
